1ff2ebf53e4266b2cb1769767429e57e14e33a96
Antoniel Magalhães <antoniel2210@gmail.com>
2020-06-25 11:18:57 -0300

    Introduction

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class BookList extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Parasita</li>
+          <li>The prince of tides</li>
+          <li>Her</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
