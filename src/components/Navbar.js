35d9c3b5955b69bd38dd85e4ca2ca36653582c26
Antoniel Magalhães <antoniel2210@gmail.com>
2020-06-27 19:31:14 -0300

    Consuming Multiple Contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends Component {
   static contextType = ThemeContext;
@@ -7,22 +8,28 @@ class Navbar extends Component {
     console.log(this.context);
 
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>Cibtext app</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(AuthContext) => (
+          <ThemeContext.Consumer>
+            {(ThemeContext) => {
+              const { isAuthenticated, toggleAuth } = AuthContext;
+              const { isLightTheme, light, dark } = ThemeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                  <h1>Cibtext app</h1>
+                  <div onClick={toggleAuth}>{isAuthenticated ? "Logged In" : "Logged Outr"}</div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -6,12 +6,12 @@ class AuthContextProvider extends Component {
   state = {
     isAuthenticated: false,
   };
-  toggelAuth = () => {
-    this.setState({ isAuthenticated: this.state.isAuthenticated });
+  toggleAuth = () => {
+    this.setState({ isAuthenticated: !this.state.isAuthenticated });
   };
   render() {
     return (
-      <AuthContext.Provider value={{ ...this.state, toggleAuth: this.toggelAuth }}>
+      <AuthContext.Provider value={{ ...this.state, toggleAuth: this.toggleAuth }}>
         {this.props.children}
       </AuthContext.Provider>
     );
