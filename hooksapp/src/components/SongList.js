b6257942a8e124950b0c9171d2b1dd49d26b9337
Antoniel Magalhães <antoniel2210@gmail.com>
2020-06-28 11:10:24 -0300

    Intro to Hooks

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>Sound of silence</li>
+        <li>Bohemia Rapsodyh</li>
+        <li>São Paulo</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
