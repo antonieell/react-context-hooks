49af118299da8e2951d6cc9e13a79da58eb639ee
Antoniel Magalhães <antoniel2210@gmail.com>
2020-06-29 20:34:14 -0300

    Reading List App (part 3)

---
+++ booklist/src/components/BookForms.js
@@ -0,0 +1,29 @@
+import React, { useContext, useState } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+const NewBookForms = () => {
+  const { addBook } = useContext(BookContext);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle("");
+    setAuthor("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input required placeholder="Book title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input
+        required
+        placeholder="Book Author"
+        type="text"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <input value="Add a new book" type="submit"></input>
+    </form>
+  );
+};
+
+export default NewBookForms;
